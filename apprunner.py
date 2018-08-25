e94a16bc71b3ae79c5ed4a80e2b11d1e4ce6e280
Rongan Li <rongan.li@stonybrook.edu>
2018-08-25 19:05:34 -0400

    successfully added the functionality of scheduling an appointment

--- apprunner.py
+++ apprunner.py
@@ -40,6 +40,8 @@ def logout():
     session.pop('logged_in', None)
     return render_template("index.html")
 
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -63,27 +65,53 @@ def setProfile(email):
     query = "select profileDirectory, firstname,lastname, profile_intro from clients  where email = " + email
     cursor.execute(query)
     profile_info = cursor.fetchall()
-    query = "SELECT skillname, charge, rateByHour, description FROM skills WHERE email = " + email
+    query = "SELECT skillname, charge, rateByHour, description, skillID FROM skills WHERE email = " + email
     cursor.execute(query)
     skill_info = cursor.fetchall()
     return render_template("profile.html", profile_info=profile_info, skill_info=skill_info,
                            profileEmail= email)
 
 
+@app.route('/appointmentMade', methods=['POST'])
+def makeAppointment():
+    appoint_info = request.form['submit']
+    date = ('"%s"' % appoint_info.split(',')[0])
+    start_time = appoint_info.split(',')[1]
+    end_time = appoint_info.split(',')[2]
+    skill_ID = appoint_info.split(',')[3]
+    street = ('"%s"' % request.form['streetAddr'])
+    aptNumber = ('"%s"' % request.form['apartNum'])
+    city = ('"%s"' % request.form['city'])
+    state = ('"%s"' % request.form['state'])
+    zip = request.form['zip']
+    cursor = conn.cursor()
+    query = 'select max(appointmentID) from appointment;'
+    cursor.execute(query)
+    appointID = cursor.fetchall()
+    appointID = str(appointID[0][0] + 1)
+    #sample query for this function: INSERT INTO appointment values (10, 'hola@das.com','sad@das.com','2018-08-12',1430,1530,'111 hola street', '3f',
+    #'Queens','NY', 10013, 9);
+    client_email = ('"%s"' % session['curr_expert_email'])
+    customer_email = ('"%s"' % session['email'])
+    query = "INSERT INTO appointment values (" + appointID + "," + client_email + "," + customer_email + ","+ date + "," + start_time + ","+ end_time + "," + street + "," + aptNumber + "," + city + "," + state + "," + zip + "," + skill_ID + ");"
+    cursor.execute(query)
+    conn.commit()
+    return render_template('appointmentMade.html')
+
+
+def getAppointId(string):
+    index_first_commas = string.find(',')
+    newString = string[2:index_first_commas]
+    return newString
+
 
 @app.route('/profile', methods=['POST'])
 def getProfile():
     profileEmail = request.form['submit']
+    session['curr_expert_email'] = profileEmail
     profileEmail = ('"%s"' % profileEmail)
     session['client_email'] = profileEmail
-    return redirect(url_for('.setProfile',email = profileEmail))
-
-
-##the following method is for testing
-@app.route('/<email>')
-def test(email):
-    return render_template("test.html", email=email)
-
+    return redirect(url_for('.setProfile', email = profileEmail))
 
 
 @app.route('/profile/schedule', methods=['POST'])
@@ -91,22 +119,23 @@ def getSchedule():
     try:
         if session['logged_in'] == False:
             return redirect(url_for('login'))
-        profileEmail = request.form['submit']
+        profileEmail = session['curr_expert_email']
+        profileEmail = ('"%s"' % profileEmail)
+        skill = request.form['submit']
         cursor = conn.cursor()
-        dateQuery = "SELECT appointmentDate FROM appointment WHERE DATEDIFF(NOW(), appointmentDate) >= - 7 AND DATEDIFF(NOW(), appointmentDate) <= 0 AND clientEmail = " + profileEmail
+        dateQuery = "SELECT appointmentDate FROM appointment WHERE DATEDIFF(NOW(), appointmentDate) >= - 7 AND DATEDIFF(NOW(), appointmentDate) <= -1 AND clientEmail = " + profileEmail
         cursor.execute(dateQuery)
         appointmentDate = cursor.fetchall()
-        startTimeQuery = "SELECT appointStartTime FROM appointment WHERE DATEDIFF(NOW(), appointmentDate) >= - 7 AND DATEDIFF(NOW(), appointmentDate) <= 0 AND clientEmail = " + profileEmail
+        startTimeQuery = "SELECT appointStartTime FROM appointment WHERE DATEDIFF(NOW(), appointmentDate) >= - 7 AND DATEDIFF(NOW(), appointmentDate) <= -1 AND clientEmail = " + profileEmail
         cursor.execute(startTimeQuery)
         appointStartTime = cursor.fetchall()
-        endTimeQuery = "SELECT appointEndTime FROM appointment WHERE DATEDIFF(NOW(), appointmentDate) >= - 7 AND DATEDIFF(NOW(), appointmentDate) <= 0 AND clientEmail = " + profileEmail
+        endTimeQuery = "SELECT appointEndTime FROM appointment WHERE DATEDIFF(NOW(), appointmentDate) >= - 7 AND DATEDIFF(NOW(), appointmentDate) <= -1 AND clientEmail = " + profileEmail
         cursor.execute(endTimeQuery)
         appointEndTime = cursor.fetchall()
         return render_template("schedule.html", appointmentDate=appointmentDate, appointStartTime=appointStartTime,
-                               appointEndTime=appointEndTime)
+                               appointEndTime=appointEndTime, skill = skill)
     except:
         return redirect(url_for('login'))
-
 
 
 if __name__ == "__main__":
--- static/js/schedule_builder.js
+++ static/js/schedule_builder.js
@@ -11,7 +11,11 @@ var timeSlot = ["800","830","900","930","1000","1030","1100","1130","1200","1230
 var dayDiffFromToday = ["today","tmr","2_days_later","3_days_later","4_days_later","5_days_later","6_days_later"];
 
 function getDayFromToday(num){
-    if(num==1){
+    if(num == 0){
+        return "Sunday";
+    }
+
+    else if(num==1){
         return "Monday";
     }
     else if(num == 2){
@@ -29,13 +33,14 @@ function getDayFromToday(num){
     else if(num == 6 ){
         return "Saturday";
     }
-    else if(num == 7){
-        return "Sunday";
-    }
 }
 
 function abbreviateDay(num){
-    if(num == 1){
+     if(num == 0){
+        return "Sun";
+    }
+
+    else if(num == 1){
         return "Mon";
     }
     else if(num == 2 ){
@@ -53,23 +58,15 @@ function abbreviateDay(num){
     else if(num == 6){
         return "Sat";
     }
-    else if(num == 7){
-        return "Sun";
-    }
 }
 
 function buildDayColumn(){
     var date = new Date();
-    var day = date.getDay();
-    document.getElementById("today_date").innerHTML = monthNames[date.getMonth()]+ "," + date.getDate();
-    document.getElementById("today_day_long").innerHTML = 'today';
-    document.getElementById("today_day_short").innerHTML = abbreviateDay(day);
-
 
     date.setDate(date.getDate() + 1);
     var day = date.getDay();
     document.getElementById("tmr_date").innerHTML = monthNames[date.getMonth()]+ "," + date.getDate();
-    document.getElementById("tmr_date_long").innerHTML = getDayFromToday(day);
+    document.getElementById("tmr_date_long").innerHTML = 'tommorrow';
     document.getElementById("tmr_date_short").innerHTML = abbreviateDay(day);
 
     date.setDate(date.getDate() + 1);
@@ -116,12 +113,19 @@ function buildDateArray(appointmentDates){
 
 function buildDayArray(dateArray){
     var dayArray = [];
+
+    //year, month, and day variables are the current year, current month, and current day correspondingly
+    var year = new Date().getFullYear();
+    var month = new Date().getMonth();
+    var day = new Date().getDate();
+
     for(i = 0; i < dateArray.length;i++){
         var dateInArray = dateArray[i].split(",");
         var date = new Date(dateInArray[0],dateInArray[1]-1,dateInArray[2]);
-        var today = new Date();
-        var dayDifference = date.getDay() - today.getDay();
-        dayArray.push(dayDiffFromToday[dayDifference]);
+        var today = new Date(year,month,day);
+        var diffDays = Math.round((date-today)/(1000*60*60*24));
+        //var dayDifference = date.getDate() -  today.getDate();
+        dayArray.push(dayDiffFromToday[diffDays]);
     }
     return dayArray;
 }
@@ -150,11 +154,266 @@ function buildAvailSchedule(dayArray, startTimeArray,endTimeArray){
         var startTimeIndex = timeSlot.indexOf(startTimeArray[i]);
         var endTimeIndex = timeSlot.indexOf(endTimeArray[i]);
 
-        for(j = startTimeIndex; j < endTimeIndex; j++){
+        for(j = startTimeIndex; j <= endTimeIndex; j++){
             var timeSlotID = dayArray[i]+ "_" + timeSlot[j];
             document.getElementById(timeSlotID).innerHTML = "unavailable";
             document.getElementById(timeSlotID).style.backgroundColor = "red";
         }
 
     }
+}
+
+
+//following elements are for the modal box upon asking for start time of appointment
+function displayModal(myModal){
+    // Get the modal
+    var modal = document.getElementById(myModal);
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    // Get the <span> element that closes the modal
+    if(myModal == 'startTimeModal'){
+        var span = document.getElementsByClassName("close")[0];
+    }
+    else if(myModal == 'endTimeModal'){
+        var span = document.getElementsByClassName("close")[1];
+    }
+
+    // When the user clicks on the button, open the modal
+    modal.style.display = "block";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+//end of elements are for the modal box upon asking for start time of appointment
+
+
+//the following code is for the clicking event, such as picking an start-time of appointment
+var table = document.getElementById("schedule");
+var appointStartDate;
+var appointEndDate;
+var appointStartTime;
+var appointEndTime;
+var currStartCell;
+var currEndCell;
+var skillID;
+
+function setSkill(skillName){
+    skillID = skillName;
+}
+
+function setAppointStart() {
+    if (table != null) {
+        for (var i = 0; i < table.rows.length; i++) {
+            for (var j = 0; j < table.rows[i].cells.length; j++)
+                table.rows[i].cells[j].onclick = function () {
+                   if(this.style.backgroundColor == 'red' || this.id == 'appointButtCell'){
+                       return;
+                   }
+                   else if(currStartCell == null){
+                       this.style.backgroundColor = 'blue';
+                       this.innerHTML = 'Start Time';
+                       currStartCell = this;
+                       appointStartDate = getAppointDate(this.id);
+                       appointStartTime = getAppointTime(this.id);
+                   }
+                   else if(currStartCell == this){
+                       this.style.backgroundColor = null;
+                       this.innerHTML = null;
+                       currStartCell = null;
+                       appointStartDate = null;
+                       appointStartTime = null;
+                   }
+                   else{
+                       currStartCell.style.backgroundColor = null;
+                       currStartCell.innerHTML = null;
+                       this.style.backgroundColor = 'blue';
+                       this.innerHTML = 'start time';
+                       currStartCell = this;
+                       appointStartDate = getAppointDate(this.id);
+                       appointStartTime = getAppointTime(this.id);
+                   }
+                };
+        }
+    }
+}
+
+function setAppointEnd() {
+    if (table != null) {
+        for (var i = 0; i < table.rows.length; i++) {
+            for (var j = 0; j < table.rows[i].cells.length; j++)
+                table.rows[i].cells[j].onclick = function () {
+                   if(this.style.backgroundColor == 'red' || this.id == 'appointButtCell'){
+                       return;
+                   }
+                   else if(this == currStartCell){
+                       return;
+                   }
+                   else if(currEndCell == null){
+                       if(getAppointDate(this.id) != appointStartDate){
+                            alert('Appointment start time and end time must be at the same day.');
+                            return;
+                       }
+                       else if(timeSlot.indexOf(getAppointTime(this.id)) < timeSlot.indexOf(appointStartTime)){
+                           alert('End time must be after start time.');
+                           return;
+                       }
+                       this.style.backgroundColor = 'blue';
+                       this.innerHTML = 'end Time';
+                       currEndCell = this;
+                       appointEndDate = getAppointDate(this.id);
+                       appointEndTime = getAppointTime(this.id);
+                       var button = document.getElementById('final_confirm_butt');
+                       button.value = appointStartDate + "," + appointStartTime + "," + appointEndTime + "," + skillID;
+                   }
+                   else if(currEndCell == this){
+                       this.style.backgroundColor = null;
+                       this.innerHTML = null;
+                       currEndCell = null;
+                       appointEndDate = null;
+                       appointEndTime = null;
+                   }
+                   else{
+                       if(getAppointDate(this.id) != appointStartDate){
+                            alert('Appointment start time and end time must be at the same day.');
+                            return;
+                       }
+                       else if(timeSlot.indexOf(getAppointTime(this.id)) < timeSlot.indexOf(appointStartTime)){
+                           alert('End time must be after start time.');
+                           return;
+                       }
+                       currEndCell.style.backgroundColor = null;
+                       currEndCell.innerHTML = null;
+                       this.style.backgroundColor = 'blue';
+                       this.innerHTML = 'end time';
+                       currEndCell = this;
+                       appointEndDate = getAppointDate(this.id);
+                       appointEndTime = getAppointTime(this.id);
+                       var button = document.getElementById('final_confirm_butt');
+                       button.value = appointStartDate + "," + appointStartTime + "," + appointEndTime + "," + skillID;
+                   }
+                };
+        }
+    }
+}
+
+function setAppointment(){
+    var button = document.getElementById('appointButt');
+    if(button.innerHTML == 'next'){
+        setAppointStart();
+    }
+    else{
+        setAppointEnd();
+    }
+}
+
+
+function processAppointment(){
+    var button = document.getElementById('appointButt');
+    if(button.innerHTML == 'next'){
+        if(appointStartTime == null || appointStartDate == null){
+            alert('Please, pick the start time of the appointment.');
+        }
+        else{
+             displayModal('endTimeModal');
+             button.innerHTML = 'cancel';
+             document.getElementById('confirmButt').style.display = "block";
+        }
+    }
+    else if(button.innerHTML == 'cancel'){
+        appointStartDate = null;
+        appointEndDate = null;
+        appointStartTime = null;
+        appointEndTime = null;
+        currStartCell.style.backgroundColor = null;
+        currStartCell.innerHTML = null;
+        currEndCell.style.backgroundColor = null;
+        currEndCell.innerHTML = null;
+    }
+}
+
+function getAppointDate(cell) {
+    var today = new Date();
+    if (cell.slice(0,5) == 'today'){
+        return null;
+    }
+    else if(cell.slice(0,3) == 'tmr'){
+        var date = new Date();
+        date.setDate(today.getDate()+1);
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1 ;
+        var day = date.getDate();
+        return year + '-' + month+ '-' + day;
+    }
+
+    else if(cell.slice(0,6) == '2_days'){
+        var date = new Date();
+        date.setDate(date.getDate() + 2);
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1 ;
+        var day = date.getDate();
+        return year + '-' + month+ '-' + day;
+    }
+
+    else if(cell.slice(0,6) == '3_days'){
+        var date = new Date();
+        date.setDate(date.getDate() + 3);
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1 ;
+        var day = date.getDate();
+        return year + '-' + month+ '-' + day;
+    }
+
+    else if(cell.slice(0,6) == '4_days'){
+        var date = new Date();
+        date.setDate(date.getDate() + 4);
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1 ;
+        var day = date.getDate();
+        return year + '-' + month+ '-' + day;
+    }
+
+    else if(cell.slice(0,6) == '5_days'){
+        var date = new Date();
+        date.setDate(date.getDate() + 5);
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1 ;
+        var day = date.getDate();
+        return year + '-' + month+ '-' + day;
+    }
+
+    else if(cell.slice(0,6) == '6_days'){
+        var date = new Date();
+        date.setDate(date.getDate() + 6);
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1 ;
+        var day = date.getDate();
+        return year + '-' + month+ '-' + day;
+    }
+}
+
+function getAppointTime(cell){
+    var index_of_last_dash = cell.lastIndexOf('_');
+    var cell_length = cell.length;
+    var time = cell.slice(index_of_last_dash + 1, cell_length);
+    return time;
+}
+
+//the following code is for the modal box of filling the address.
+var states = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia",
+    "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan","Minnesota", "Missouri", "Mississippi", "Montana",
+    "North Carolina", " North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma",
+    "Oregon", "Pennsylvania","Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"];
+
+for(i = 0; i < states.length;i++){
+    var box = document.getElementById('stateBox');
+    var option = document.createElement("option");
+    option.text = states[i];
+    box.add(option);
 }
